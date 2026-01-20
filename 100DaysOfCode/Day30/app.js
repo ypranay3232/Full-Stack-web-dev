3451624f28f97526786c2b975b64d2602671067c
ypranay3232 <ypranay56@gmail.com>
2026-01-20 11:07:21 +0530

    Day 30

---
+++ 100DaysOfCode/Day30/app.js
@@ -0,0 +1,17 @@
+// alert('Hello world')
+// let demo = "This is from let"
+// alert(demo)
+// demo = "This is not from let but from demo itself"
+// alert(demo)
+
+const { title } = require("process")
+
+let name = 'demo'
+let age = 24
+
+// arrays
+let hobbies = ['sports','cooking','reading']
+alert(hobbies)
+
+// objects 
+let job = {title: 'Developer', place : 'Remote', salary : '70k$'}
