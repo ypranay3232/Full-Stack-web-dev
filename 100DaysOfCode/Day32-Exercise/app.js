ae280b3f7b39b634382179cd05485ae711574844
ypranay3232 <ypranay56@gmail.com>
2026-01-21 18:05:48 +0530

    Day 32- updated app.js

--- 100DaysOfCode/Day32-Exercise/app.js
+++ 100DaysOfCode/Day32-Exercise/app.js
@@ -21,3 +21,32 @@ result--
 result+= 1
 result-= 1
 console.log(result)
+
+
+// String operations 
+// string concatenation 
+console.log('max' + 'tennyson')
+
+// to get the length of a string we use length method
+let username = 'This is a demo text'
+console.log(username.length)
+
+// we can also access a string with index
+console.log(username[2])
+
+// strings slicing : slice()
+console.log(username.slice(1,3))
+
+// substring() : the only difference between slice () and substring is the substring assumes values as 0 if any start and end values are less than 0
+// This assumes -1 as 0 and starts from 0 to 4 (n-1)
+console.log(username.substring(-1,5))
+
+// Converting to uppercase and lowercase 
+console.log(username.toUpperCase())
+console.log(username.toLowerCase())
+
+// to remove whitespaces from a string we use trim() this removes whitespaces from both ends and also another method called trimstart() this only removes whitespaces from start
+let whitespaces_Test = "         demo   "
+console.log(whitespaces_Test.trim())
+console.log(whitespaces_Test.trimStart())
+console.log(whitespaces_Test.trimEnd())
