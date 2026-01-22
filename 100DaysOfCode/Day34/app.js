1ef1dc0abaf5d75f3494c2bc62fe4cef15608c5d
ypranay3232 <ypranay56@gmail.com>
2026-01-22 16:18:50 +0530

    Day 34

---
+++ 100DaysOfCode/Day34/app.js
@@ -0,0 +1,8 @@
+let anchorelement = document.getElementById('link')
+anchorelement.href = 'https://developer.mozilla.org/en-US/'
+anchorelement.textContent = 'Go to MDN'
+// or we can also use query selector 
+
+// anchorelement1 = document.querySelector('a')
+// anchorelement1.href = 'https://www.google.com'
+// anchorelement1.textContent = 'Go to Google'
