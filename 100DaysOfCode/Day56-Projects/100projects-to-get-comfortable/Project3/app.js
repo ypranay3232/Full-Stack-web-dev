47f1a425e893789ef2ea588569c9ab323bd85226
ypranay3232 <ypranay56@gmail.com>
2026-02-21 18:01:55 +0530

    updated issue : 12 hours conversion

--- 100DaysOfCode/Day56-Projects/100projects-to-get-comfortable/Project3/app.js
+++ 100DaysOfCode/Day56-Projects/100projects-to-get-comfortable/Project3/app.js
@@ -8,22 +8,24 @@ function displayTime(){
     // now select the session : 
     let session = document.getElementById("session")
 
-    // now select the hours mins and seconds
-    document.getElementById("hours").innerHTML = hrs
-    document.getElementById("mins").innerHTML = mins
-    document.getElementById("sec").innerHTML= sec
-
+    
     // now Update the session :
     if(hrs >=12){
         session.innerHTML = 'PM'
     }else{
         session.innerHTML = "AM"
     }
-
+    
     // to get 12 hours format 
     if(hrs >12){
         hrs = hrs - 12
     }
+    // Just realized one thing i'm updating the value before converting 24 hours into 12 hrs so to fix i just have to update it after converting the values thats it.
+    
+    // now select the hours mins and seconds
+    document.getElementById("hours").innerHTML = hrs
+    document.getElementById("mins").innerHTML = mins
+    document.getElementById("sec").innerHTML= sec
 }
 // now set an interval of 1 sec(10ms) to update the clock 
 setInterval(displayTime,10)
