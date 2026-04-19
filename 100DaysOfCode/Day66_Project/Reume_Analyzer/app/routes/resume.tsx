69300772623067b41ef0e6260f443ce0c252bf58
ypranay3232 <ypranay56@gmail.com>
2026-04-19 15:38:36 +0530

    removed some warnings

--- 100DaysOfCode/Day66_Project/Reume_Analyzer/app/routes/resume.tsx
+++ 100DaysOfCode/Day66_Project/Reume_Analyzer/app/routes/resume.tsx
@@ -65,7 +65,7 @@ const Resume = () => {
     }, [id, puterReady]);
 
     return (
-        <main className="!pt-0">
+        <main className="pt-0!">
             <nav className="resume-nav">
                 <Link to="/" className="back-button">
                     <img src="/icons/back.svg" alt="logo" className="w-2.5 h-2.5" />
@@ -73,7 +73,7 @@ const Resume = () => {
                 </Link>
             </nav>
             <div className="flex flex-row w-full max-lg:flex-col-reverse">
-                <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[100vh] sticky top-0 items-center justify-center">
+                <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-screen sticky top-0 items-center justify-center">
                     {imageUrl && resumeUrl && (
                         <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
                             <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
@@ -87,7 +87,7 @@ const Resume = () => {
                     )}
                 </section>
                 <section className="feedback-section">
-                    <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
+                    <h2 className="text-4xl text-black! font-bold">Resume Review</h2>
                     {errorMsg ? (
                         <div className="flex flex-col items-center justify-center p-10 text-red-500 text-center font-bold text-xl">
                             <p>{errorMsg}</p>
