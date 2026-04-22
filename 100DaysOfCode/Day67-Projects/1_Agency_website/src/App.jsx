4a4658a5545482b9c128e5bdba726417d0e926fd
ypranay3232 <ypranay56@gmail.com>
2026-04-22 12:58:02 +0530

    worked on herosection

--- 100DaysOfCode/Day67-Projects/1_Agency_website/src/App.jsx
+++ 100DaysOfCode/Day67-Projects/1_Agency_website/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import NavBar from './components/NavBar'
 import CustomCursor from './components/CustomCursor'
+import Hero from './components/Hero'
+import Trustedby from './components/Trustedby'
 
 const App = () => {
 
@@ -11,6 +13,8 @@ const App = () => {
     <div className='dark:bg-black relative'>
       <CustomCursor />
       <NavBar theme={theme} setTheme={setTheme} />
+      <Hero/>
+      <Trustedby/>
     </div>
   )
 }
