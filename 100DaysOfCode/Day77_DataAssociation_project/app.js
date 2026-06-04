14d7802bdfc275f917243a6610b106b8226a2de3
ypranay3232 <ypranay56@gmail.com>
2026-06-04 05:47:03 +0530

    created usermodel

--- 100DaysOfCode/Day77_DataAssociation_project/app.js
+++ 100DaysOfCode/Day77_DataAssociation_project/app.js
@@ -1,8 +1,17 @@
 const express = require("express")
 const app = express()
+const userModel = require('./models/usermodel')
+const cookieParser = require("cookie-parser")
+
+// setting up middleware
+app.set("view engine","ejs")
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser ())
 
 app.get("/",(req,res)=>{
-    res.send("working")
+    // res.send("working")
+    res.render("index.ejs")
 })
 
 app.listen(3000)
