fa9e0a95d6de2f656e66b4e460a5b210de466484
ypranay3232 <ypranay56@gmail.com>
2026-06-07 10:46:14 +0530

    updated model folder

--- 100DaysOfCode/Day77_DataAssociation_project/models/usermodel.js
+++ 100DaysOfCode/Day77_DataAssociation_project/models/usermodel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 mongoose.connect("mongodb://127.0.0.1:27017/Project")
     .then(() => console.log("Connected to MongoDB successfully."))
     .catch(err => {
-        console.error("❌ MongoDB connection error: Make sure MongoDB is running on your machine (127.0.0.1:27017)!");
+        console.error("MongoDB connection error: Make sure MongoDB is running on your machine (127.0.0.1:27017)!");
         console.error(err.message);
     });
 
