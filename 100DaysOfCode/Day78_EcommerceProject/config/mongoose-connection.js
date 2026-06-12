bb2da703b16e5f7b3eda8038faeea40110ce15a0
ypranay3232 <ypranay56@gmail.com>
2026-06-12 10:06:25 +0530

    fixed : mongodb connection

--- 100DaysOfCode/Day78_EcommerceProject/config/mongoose-connection.js
+++ 100DaysOfCode/Day78_EcommerceProject/config/mongoose-connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 // also handling the error cases when nodejs wont connect with mongodb
-mongoose.connect("mongoose://127.0.0.1:27017/Ecommerce_Store")//localhost connection
+mongoose.connect("mongodb://127.0.0.1:27017/Ecommerce_Store")//localhost connection
 .then(function () { console.log("connected") })
 .catch((err)=>{
     console.log(err);
