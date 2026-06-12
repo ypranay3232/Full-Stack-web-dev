76a2aed9822edb1db759c6a570b5c3da5ec069a8
ypranay3232 <ypranay56@gmail.com>
2026-06-12 10:06:56 +0530

    updated app.js

--- 100DaysOfCode/Day78_EcommerceProject/app.js
+++ 100DaysOfCode/Day78_EcommerceProject/app.js
@@ -5,6 +5,9 @@ const CookieParser  = require("cookie-parser")
 const path = require('path')
 
 const db = require("./config/mongoose-connection")
+const OwnersRoute = require("./routes/OwnersRoute")
+const ProductsRoute = require("./routes/ProductsRoute")
+const UsersRoute = require("./routes/UsersRoute")
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -12,9 +15,8 @@ app.use(CookieParser())
 app.use(express.static(path.join(__dirname,"public")))
 app.set("view engine","ejs")
 
-app.get("/",(req,res)=>{
-    res.send("initial setup")
-
-})
+app.use("/owners",OwnersRoute)
+app.use("/users",UsersRoute)
+app.use("/products",ProductsRoute)
 
 app.listen(3000)
