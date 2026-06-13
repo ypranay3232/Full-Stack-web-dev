963965fd98ff1a93e505e7416e1135c4a591b91b
ypranay3232 <ypranay56@gmail.com>
2026-06-13 06:30:04 +0530

    Updated with best practices created a development.json

--- 100DaysOfCode/Day78_EcommerceProject/config/mongoose-connection.js
+++ 100DaysOfCode/Day78_EcommerceProject/config/mongoose-connection.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
+const config = require('config')
+// learning how to use a debugger : npm i debug
+const debgr = require('debug')("development:mongoose")
 
 // also handling the error cases when nodejs wont connect with mongodb
-mongoose.connect("mongodb://127.0.0.1:27017/Ecommerce_Store")//localhost connection
-.then(function () { console.log("connected") })
+mongoose.connect(`${config.get("MONGODB_URI")}/Ecommerce_store`)//localhost connection : changed to env variables
+.then(function () { debgr("connected") })
 .catch((err)=>{
-    console.log(err);
+    debgr(err);//this wont print anything until we set env variables. set DEBUG=development:*
 })
 
 module.exports = mongoose.connection
