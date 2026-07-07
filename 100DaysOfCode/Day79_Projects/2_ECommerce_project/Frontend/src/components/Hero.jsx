97b6ae64e2b53e0d15e5f4c719ef7e8908a9429c
ypranay3232 <ypranay56@gmail.com>
2026-07-07 21:26:00 +0530

    starting Hero page

---
+++ 100DaysOfCode/Day79_Projects/2_ECommerce_project/Frontend/src/components/Hero.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { assets } from "../assets/assets"
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col sm:flex-row border border-gray-400'>
+        {/* we create 2 section hero left and right */}
+        <div className='w-full  sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
+            <div className='text-[#414141]'>
+                <div className='flex items-center gap-2'>
+                    {/* we use this p tag as a line */}
+                    <p className='w-8 md:w-11 h-0.5 bg-[#414141]'></p>
+                    <p className='font-medium text-sm md:text-base'>OUR BEST SELLERS</p>
+                </div>
+                <h1 className='text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrivals</h1>
+                <div className='flex items-center gap-2'>
+                    <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
+                    <p className='w-8 md:w-11 h-0.5 bg-[#414141]'></p>
+                </div>
+            </div>
+        </div>
+
+        {/* hero right side which has an image*/}
+        <img src={assets.hero_img} className='w-full sm:w-1/2' alt="" />
+
+    </div>
+  )
+}
+
+export default Hero
